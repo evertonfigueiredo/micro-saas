45aebe55af206b2757cdd16c1ea1af7f5fe5a341
Everton Figueiredo <j.everton.figueiredo@gmail.com>
2024-05-04 12:54:34 -0300

    sistema de atualização do stripe

--- src/config.ts
+++ src/config.ts
@@ -1,20 +1,19 @@
 export const config = {
   stripe: {
-    publishableKey: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
-    secretKey: process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY,
-    webhookSecret:
-      'whsec_17a14e2622bead06a4132460bb6ea5a4a9b3a14e8c81f7cb7ebaa73f4cd770e5',
+    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    secretKey: process.env.STRIPE_SECRET_KEY,
+    webhookSecret: process.env.STRIPE_WEBHOOK_SECRET,
     plans: {
       free: {
         priceId: 'price_1PBM40P8AxlGqivH7xYBggbp',
         quota: {
-          TASKS: 5,
+          TASKS: 100,
         },
       },
       pro: {
         priceId: 'price_1PBM4EP8AxlGqivHap08O7Kf',
         quota: {
-          TASKS: 100,
+          TASKS: 1000,
         },
       },
     },
--- src/services/stripe/index.ts
+++ src/services/stripe/index.ts
@@ -48,31 +48,172 @@ export const createStripeCustomer = async (input: {
 export const createCheckoutSession = async (
   userId: string,
   userEmail: string,
+  userStripeSubscriptionId: string,
 ) => {
   try {
     const customer = await createStripeCustomer({
       email: userEmail,
     })
 
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
-      mode: 'subscription',
-      client_reference_id: userId,
-      customer: customer.id,
-      success_url: `http://localhost:3000/app/settings/billing?success=true`,
-      cancel_url: `http://localhost:3000/app/settings/billing?success=false`,
-      line_items: [
-        {
-          price: config.stripe.plans.pro.priceId,
-          quantity: 1,
-        },
-      ],
+    const subscription = await stripe.subscriptionItems.list({
+      subscription: userStripeSubscriptionId,
+      limit: 1,
     })
+
+    const session = await stripe.billingPortal.sessions.create({
+      customer: customer.id,
+      return_url: 'http://localhost:3000/app/settings/billing',
+      flow_data: {
+        type: 'subscription_update_confirm',
+        after_completion: {
+          type: 'redirect',
+          redirect: {
+            return_url:
+              'http://localhost:3000/app/settings/billing?success=true',
+          },
+        },
+        subscription_update_confirm: {
+          subscription: userStripeSubscriptionId,
+          items: [
+            {
+              id: subscription.data[0].id,
+              price: config.stripe.plans.pro.priceId,
+              quantity: 1,
+            },
+          ],
+        },
+      },
+    })
+
+    // Cria assinatura sem atualizar o plano
+    // const session = await stripe.checkout.sessions.create({
+    //   payment_method_types: ['card'],
+    //   mode: 'subscription',
+    //   client_reference_id: userId,
+    //   customer: customer.id,
+    //   success_url: `http://localhost:3000/app/settings/billing?success=true`,
+    //   cancel_url: `http://localhost:3000/app/settings/billing?success=false`,
+    //   line_items: [
+    //     {
+    //       price: config.stripe.plans.pro.priceId,
+    //       quantity: 1,
+    //     },
+    //   ],
+    // })
 
     return {
       url: session.url,
     }
   } catch (error) {
     throw new Error('Error to create checkout session')
+  }
+}
+
+export const handleProcessWebhookUpdatedSubscription = async (event: {
+  object: Stripe.Subscription
+}) => {
+  const stripeCustomerId = event.object.customer as string
+  const stripeSubscriptionId = event.object.id as string
+  const stripeSubscriptionStatus = event.object.status
+  const stripePriceId = event.object.items.data[0].price.id
+
+  const userExists = await prisma.user.findFirst({
+    where: {
+      OR: [
+        {
+          stripeSubscriptionId,
+        },
+        {
+          stripeCustomerId,
+        },
+      ],
+    },
+    select: {
+      id: true,
+    },
+  })
+
+  if (!userExists) {
+    throw new Error('user of stripeCustomerId not found')
+  }
+
+  await prisma.user.update({
+    where: {
+      id: userExists.id,
+    },
+    data: {
+      stripeCustomerId,
+      stripeSubscriptionId,
+      stripeSubscriptionStatus,
+      stripePriceId,
+    },
+  })
+}
+
+type Plan = {
+  priceId: string
+  quota: {
+    TASKS: number
+  }
+}
+
+type Plans = {
+  [key: string]: Plan
+}
+
+export const getPlanByPrice = (priceId: string) => {
+  const plans: Plans = config.stripe.plans
+
+  const planKey = Object.keys(plans).find(
+    (key) => plans[key].priceId === priceId,
+  ) as keyof Plans | undefined
+
+  const plan = planKey ? plans[planKey] : null
+
+  if (!plan) {
+    throw new Error(`Plan not found for priceId: ${priceId}`)
+  }
+
+  return {
+    name: planKey,
+    quota: plan.quota,
+  }
+}
+
+export const getUserCurrentPlan = async (userId: string) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+    select: {
+      stripePriceId: true,
+    },
+  })
+
+  if (!user || !user.stripePriceId) {
+    throw new Error('Esse usuário não encontrou stripePriceId')
+  }
+
+  const plan = getPlanByPrice(user.stripePriceId)
+
+  const tasksCount = await prisma.todo.count({
+    where: {
+      userId,
+    },
+  })
+
+  const availableTasks = plan.quota.TASKS
+  const currentTasks = tasksCount
+  const usage = (currentTasks / availableTasks) * 100
+
+  return {
+    name: plan.name,
+    quota: {
+      TASKS: {
+        available: availableTasks,
+        current: currentTasks,
+        usage,
+      },
+    },
   }
 }
