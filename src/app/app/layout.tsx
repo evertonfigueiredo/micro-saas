f487e26f13d625fe73dc49997887f1a99c3e72e6
Everton Figueiredo <j.everton.figueiredo@gmail.com>
2024-05-06 12:55:40 -0300

    Type User layout

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -2,12 +2,37 @@ import { PropsWithChildren } from 'react'
 import { MainSidebar } from './_components/main-sidebar'
 import { auth } from '@/services/auth'
 
+interface User {
+  id: string
+  name: string | null
+  email: string | null
+  emailVerified: Date | null
+  image: string | null
+  stripeCustomerId: string | null
+  stripeSubscriptionId: string | null
+  stripeSubscriptionStatus: string | null
+  stripePriceId: string | null
+}
+
 export default async function Layout({ children }: PropsWithChildren) {
   const session = await auth()
 
+  // Verificar se session?.user é definido antes de passá-lo para MainSidebar
+  const user: User = session?.user || {
+    id: '',
+    name: null,
+    email: null,
+    emailVerified: null,
+    image: null,
+    stripeCustomerId: null,
+    stripeSubscriptionId: null,
+    stripeSubscriptionStatus: null,
+    stripePriceId: null,
+  }
+
   return (
     <div className="grid grid-cols-[15rem_1fr]">
-      <MainSidebar user={session?.user} />
+      <MainSidebar user={user} />
       <main>{children}</main>
     </div>
   )
