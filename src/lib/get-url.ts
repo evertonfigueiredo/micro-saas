cbef37e02cd456a25e3bde968ffb9f0ffdea245c
Everton Figueiredo <j.everton.figueiredo@gmail.com>
2024-05-06 16:00:33 -0300

    atualizar get url

--- src/lib/get-url.ts
+++ src/lib/get-url.ts
@@ -1,6 +1,15 @@
 export function getUrl(path?: string) {
   const baseUrl = process.env.NEXT_PUBLIC_APP_URL || ''
   const normalizedPath = path && !path.startsWith('/') ? `/${path}` : path || ''
-  console.log(normalizedPath, baseUrl)
-  return `${baseUrl}${normalizedPath}`
+
+  // Verificar se baseUrl é uma URL absoluta válida
+  const isAbsoluteUrl = /^https?:\/\//i.test(baseUrl)
+
+  // Se baseUrl já for uma URL absoluta válida, retornar a URL completa
+  if (isAbsoluteUrl) {
+    return `${baseUrl}${normalizedPath}`
+  } else {
+    // Caso contrário, retornar uma URL absoluta válida com um esquema padrão (por exemplo, "https://")
+    return `https://${baseUrl}${normalizedPath}`
+  }
 }
