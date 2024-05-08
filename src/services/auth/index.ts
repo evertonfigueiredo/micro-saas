d9e8c2f2ccca4bd82ff9c93483f62c360d367cf2
Everton Figueiredo <j.everton.figueiredo@gmail.com>
2024-05-08 15:24:36 -0300

    Atualização de Port

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -21,8 +21,8 @@ export const { auth, handlers } = NextAuth({
     GitHub,
     Nodemailer({
       server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
+        host: process.env.EMAIL_SERVER_HOST as string,
+        port: parseInt(process.env.EMAIL_SERVER_PORT as string) || 0,
         auth: {
           user: process.env.EMAIL_SERVER_USER,
           pass: process.env.EMAIL_SERVER_PASSWORD,
